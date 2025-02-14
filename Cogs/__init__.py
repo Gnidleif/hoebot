88547cf2010a8e814316ed566ee68d272fac83ad
Gnidleif <gnidleif@gmail.com>
2025-02-14 10:21:18 +0100

    Moved Python to separate branch

--- Cogs/__init__.py
+++
@@ -1,1 +0,0 @@
-__all__ = ["membercog", "messagecog", "randomcog"]
--- Cogs/messagecog.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3.12
-from discord import Member
-from discord.ext import commands
-
-class Messages(commands.Cog):
-    def __init__(self, bot: commands.Bot):
-        self.bot = bot
